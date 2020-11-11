7f22c2cce6659e256b28323cb8763b75201c447b
Eric Smith <eric.c.smith@sbcglobal.net>
2020-11-10 19:21:41 -0600

    first cut at adding sessions logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
     def create
         @buyer = Buyer.find_by(name: params[:name])
         return head(:forbidden) unless @buyer.authenticate(params[:password])
         session[:buyer_id] = @buyer.id
-      end
+    end
+
+    def destroy
+      session.delete :name
+      redirect_to (login page?), action: (?)
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   # post 'sellers'
 
   resources :vehicles
-
   resources :purchases
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
 
   post "/vehicles/:id/make_purchase", to: 'vehicles#make_purchase'
 
